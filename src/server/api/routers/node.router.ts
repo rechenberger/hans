33b83e8dec229c751758b8e0d692d9dbf984580c
Tristan Rechenberger <rechenberger@me.com>
2023-06-25 14:09:45 +0200

    🎉

--- src/server/api/routers/node.router.ts
+++ src/server/api/routers/node.router.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client'
+import { type PrismaClient } from '@prisma/client'
 import { first } from 'lodash-es'
 import { z } from 'zod'
 import { openai } from '~/server/ai/openai'
@@ -43,24 +43,44 @@ export const nodeRouter = createTRPCRouter({
     .mutation(async ({ ctx, input }) => {
       const node = await getNode({ prisma: ctx.prisma, id: input.id })
 
-      const response = await openai.createChatCompletion({
-        model: 'gpt-3.5-turbo-0613',
-        messages: [
-          {
-            role: 'system',
-            content: `The user is playing a video game. The player has a single item in their inventory: "${node.metadata.title}". You will give the player 3 choices of what to trade the item for. It should be a reasonable trade.`,
-          },
-          // {
-          //   role: 'user',
-          //   content: `${node.metadata.title}`,
-          // },
-        ],
-        // function_call: 'auto',
-      })
+      try {
+        const response = await openai.createChatCompletion({
+          model: 'gpt-3.5-turbo-0613',
+          messages: [
+            {
+              role: 'system',
+              content: `The user is playing a video game. The player has a single item in their inventory: "${node.metadata.title}". You will give the player 3 choices of what to trade the item for. It should be a reasonable trade. Use the GenerateItem function.`,
+            },
+            // {
+            //   role: 'user',
+            //   content: `${node.metadata.title}`,
+            // },
+          ],
+          function_call: 'auto',
+          functions: [
+            {
+              name: 'GenerateItem',
+              description: 'Generate an item',
+              parameters: {
+                type: 'array',
+                items: {
+                  type: 'string',
+                },
+              },
+            },
+          ],
+        })
+        const message = first(response.data.choices)?.message
 
-      const message = first(response.data.choices)?.message
-
-      console.log({ message })
+        console.log({ message })
+      } catch (error: any) {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+        const openAIError = error?.response?.data?.error
+        if (openAIError) {
+          console.error(openAIError)
+        }
+        throw error
+      }
     }),
 })
 
