2bd3b5c1dcdcecc1d38fa7a43966edb8bd536a91
Tristan Rechenberger <rechenberger@me.com>
2023-06-25 15:28:20 +0200

    🎉

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ export default function Page() {
     <>
       <Head>
         <title>Hans</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/hanns.png" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-br from-cyan-500 to-amber-600">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
