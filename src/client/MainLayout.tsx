81722341be2a8d9f4bdbb3c863d071282844590b
Tristan Rechenberger <rechenberger@me.com>
2023-06-25 18:17:48 +0200

    🎉

--- src/client/MainLayout.tsx
+++ src/client/MainLayout.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import { type ReactNode } from 'react'
 
 export const MainLayout = ({
@@ -14,7 +15,16 @@ export const MainLayout = ({
         <title>{title ? `${title} | Hans AI` : 'Hans AI'}</title>
         <link rel="icon" href="/hanns.png" />
       </Head>
-      <div className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-br from-cyan-500 to-amber-600">
+      <div className="flex min-h-screen flex-col items-center justify-start bg-gradient-to-br from-cyan-500 to-amber-600">
+        <div className="container flex flex-row py-8">
+          <Link
+            href="/"
+            className="text-xl font-extrabold tracking-tight text-white"
+          >
+            Hans AI
+          </Link>
+          <div className="flex-1" />
+        </div>
         <main className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           {children}
         </main>
