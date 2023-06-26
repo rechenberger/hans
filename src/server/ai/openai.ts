31848986da7d68c1eda1d8bd80b48535f18daa7a
Tristan Rechenberger <rechenberger@me.com>
2023-06-26 10:31:52 +0200

    ✨ Billable

--- src/server/ai/openai.ts
+++ src/server/ai/openai.ts
@@ -1,5 +1,15 @@
 import { Configuration, OpenAIApi } from 'openai'
+import { z } from 'zod'
 import { env } from '~/env.mjs'
+
+export const openAIModels = [
+  'gpt-4-0613',
+  'gpt-3.5-turbo-0613',
+  'gpt-3.5-turbo-16k',
+] as const
+export const openAIModelEnum = z.enum(openAIModels)
+
+export type OpenAIModel = z.infer<typeof openAIModelEnum>
 
 const configuration = new Configuration({
   apiKey: env.OPENAI_API_KEY,
---
+++ src/server/lib/BillableApiData.ts
@@ -0,0 +1,133 @@
+import { type OpenAIModel } from '../ai/openai'
+const DEFAULT_TEAMTOKEN_PER_UPSTREAM_CENT = 50
+
+export type BillableApiData =
+  | {
+      service: 'openai'
+      model: OpenAIModel
+      prompt_tokens: number
+      completion_tokens: number
+      total_tokens: number
+    }
+  // | {
+  //     service: 'openai' | 'azure'
+  //     model: 'whisper' | 'speechToText'
+  //     speechToTextAudioDuration: number
+  //     numberOfCharacters: number
+  //   }
+  // | {
+  //     service: 'openai' | 'elevenLabs' | 'azure'
+  //     model: 'textToSpeech'
+  //     speechToTextAudioDuration: number
+  //     numberOfCharacters: number
+  //   }
+  | {
+      service: 'replicate'
+      model: 'imageGeneration' | 'imageUpscaling'
+      timeToCompleteInSeconds: number
+    }
+
+// const ONE_HOUR_IN_SECONDS = 3600
+
+export const calcUpstreamCostsInCents = (billableApiData: BillableApiData) => {
+  if (billableApiData.service === 'openai') {
+    // FROM: https://openai.com/pricing
+    if (billableApiData.model === 'gpt-4-0613') {
+      const prompt = (billableApiData.prompt_tokens / 1000) * 3
+      const completion = (billableApiData.completion_tokens / 1000) * 6
+      const costsInCents = prompt + completion
+
+      return costsInCents
+    }
+    // if (billableApiData.model === 'gpt-4-32k') {
+    //   const prompt = (billableApiData.prompt_tokens / 1000) * 6
+    //   const completion = (billableApiData.completion_tokens / 1000) * 12
+    //   const costsInCents = prompt + completion
+
+    //   return costsInCents
+    // }
+    if (billableApiData.model === 'gpt-3.5-turbo-0613') {
+      const prompt = (billableApiData.prompt_tokens / 1000) * 0.15
+      const completion = (billableApiData.completion_tokens / 1000) * 0.2
+      const costsInCents = prompt + completion
+      return costsInCents
+    }
+    if (billableApiData.model === 'gpt-3.5-turbo-16k') {
+      const prompt = (billableApiData.prompt_tokens / 1000) * 0.3
+      const completion = (billableApiData.completion_tokens / 1000) * 0.4
+      const costsInCents = prompt + completion
+      return costsInCents
+    }
+
+    // if (billableApiData.model === 'whisper') {
+    //   const costsInCents =
+    //     (billableApiData.speechToTextAudioDuration / ONE_HOUR_IN_SECONDS) * 36
+    //   return costsInCents
+    // }
+    throw new Error(`Unknown openai model ${billableApiData.model}`)
+  }
+
+  // if (billableApiData.service === 'elevenLabs') {
+  //   if (billableApiData.model === 'textToSpeech') {
+  //     const costsInCents = (billableApiData.numberOfCharacters / 1000) * 30
+  //     return costsInCents
+  //   }
+  // }
+  // if (billableApiData.service === 'azure') {
+  //   if (billableApiData.model === 'textToSpeech') {
+  //     //https://azure.microsoft.com/en-us/pricing/details/cognitive-services/speech-services/
+  //     const costsInCents =
+  //       (billableApiData.numberOfCharacters / 1_000_000) * 1600
+  //     return costsInCents
+  //   }
+
+  //   if (billableApiData.model === 'speechToText') {
+  //     //https://azure.microsoft.com/en-us/pricing/details/cognitive-services/speech-services/
+  //     const costsInCents =
+  //       (billableApiData.speechToTextAudioDuration / ONE_HOUR_IN_SECONDS) * 100
+  //     return costsInCents
+  //   }
+  //   throw new Error(`Unknown azure model ${billableApiData.model}`)
+  // }
+
+  if (billableApiData.service === 'replicate') {
+    if (billableApiData.model === 'imageGeneration') {
+      const costsInCents = billableApiData.timeToCompleteInSeconds * 0.23 // $0.0023 per second - NvidiA A100 40GB https://replicate.com/ai-forever/kandinsky-2#performance
+      return costsInCents
+    }
+    if (billableApiData.model === 'imageUpscaling') {
+      const costsInCents = billableApiData.timeToCompleteInSeconds * 0.055 // $0.00055 per second - NvidiA T4 16GB // https://replicate.com/nightmareai/real-esrgan#performance
+      return costsInCents
+    }
+    throw new Error(`Unknown replicate model ${billableApiData.model}`)
+  }
+
+  // @ts-expect-error billableApiData is `never` because of the exhaustive check above
+  throw new Error(`Unknown service ${billableApiData.service}`)
+}
+
+export const calcCostsInTeamTokens = ({
+  upstreamCostsInCents,
+}: {
+  upstreamCostsInCents: number
+}) => {
+  const costsInTeamTokens = Math.ceil(
+    upstreamCostsInCents * DEFAULT_TEAMTOKEN_PER_UPSTREAM_CENT
+  )
+  return costsInTeamTokens
+}
+
+export const calcBillable = ({
+  billableApiData,
+}: {
+  billableApiData: BillableApiData
+}) => {
+  const upstreamCostsInCents = calcUpstreamCostsInCents(billableApiData)
+  const costsInTeamTokens = calcCostsInTeamTokens({
+    upstreamCostsInCents,
+  })
+  return {
+    upstreamCostsInCents,
+    costsInTeamTokens,
+  }
+}
